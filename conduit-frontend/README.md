2e2aed6daf3fc420f650a3ccd19423a8cb856cb0
Shekhar Gulati <shekhargulati84@gmail.com>
2019-01-17 03:13:06 +0530

    project init

---
+++ conduit-frontend/README.md
@@ -0,0 +1,56 @@
+[![RealWorld Frontend](https://img.shields.io/badge/realworld-frontend-%23783578.svg)](http://realworld.io)
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+# ![RealWorld Example App](./static/rwv-logo.png)
+
+> Vue.js codebase containing real world examples (CRUD, auth, advanced patterns, etc) that adheres to the [RealWorld](https://github.com/gothinkster/realworld) spec and API.
+
+Project demo is available at https://vue-vuex-realworld.netlify.com/#/
+
+This codebase was created to demonstrate a fully fledged fullstack application built with **Vue.js** including CRUD operations, authentication, routing, pagination, and more.
+
+We've gone to great lengths to adhere to the **Vue.js** community styleguides & best practices.
+
+For more information on how to this works with other frontends/backends, head over to the [RealWorld](https://github.com/gothinkster/realworld) repo.
+
+## Getting started
+
+Before contributing please read the following:
+
+1. [RealWorld guidelines](https://github.com/gothinkster/realworld/tree/master/spec) for implementing a new framework,
+2. [RealWorld frontend instructions](https://github.com/gothinkster/realworld-starter-kit/blob/master/FRONTEND_INSTRUCTIONS.md)
+3. [Realworld API endpoints](https://github.com/gothinkster/realworld/tree/master/api)
+4. [Vue.js styleguide](https://vuejs.org/v2/style-guide/index.html). Priority A and B categories must be respected.
+
+The stack is built using [vue-cli webpack](https://github.com/vuejs-templates/webpack) so to get started all you have to do is:
+
+``` bash
+# install dependencies
+> yarn install
+# serve with hot reload at localhost:8080
+> yarn serve
+```
+
+Other commands available are:
+
+``` bash
+# build for production with minification
+yarn run build
+
+# run unit tests
+yarn run test:unit
+```
+
+# To know
+
+Current arbitrary choices are:
+
+- Vuex modules for store
+- Vue-axios for ajax requests
+- 'rwv' as prefix for components
+
+These can be changed when the contributors reach a consensus.
+
+# Connect
+
+Join us on [Discord](https://discord.gg/NE2jNmg)
---
+++ conduit-frontend/src/components/ArticleActions.vue
@@ -0,0 +1,92 @@
+<template>
+  <!-- Used when user is also author -->
+  <span v-if="canModify">
+    <router-link
+      class="btn btn-sm btn-outline-secondary"
+      :to="{ name: 'article-edit', params: { slug: this.article.slug } }"
+    >
+      <i class="ion-edit"></i><span>&nbsp;Edit Article</span>
+    </router-link>
+    <span>&nbsp;&nbsp;</span>
+    <button class="btn btn-outline-danger btn-sm" @click="deleteArticle">
+      <i class="ion-trash-a"></i><span>&nbsp;Delete Article</span>
+    </button>
+  </span>
+  <!-- Used in ArticleView when not author -->
+  <span v-else>
+    <button class="btn btn-sm btn-outline-secondary" @click="toggleFollow">
+      <i class="ion-plus-round"></i> <span>&nbsp;</span>
+      <span
+        >{{ profile.following ? "Unfollow" : "Follow" }}
+        {{ article.author.username }}</span
+      >
+    </button>
+    <span>&nbsp;&nbsp;</span>
+    <button
+      class="btn btn-sm"
+      @click="toggleFavorite"
+      :class="{
+        'btn-primary': article.favorited,
+        'btn-outline-primary': !article.favorited
+      }"
+    >
+      <i class="ion-heart"></i><span>&nbsp;</span>
+      <span>
+        {{ article.favorited ? "Unfavorite Article" : "Favorite Article" }}
+      </span>
+      <span class="counter"> ({{ article.favoritesCount }}) </span>
+    </button>
+  </span>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import {
+  FAVORITE_ADD,
+  FAVORITE_REMOVE,
+  ARTICLE_DELETE,
+  FETCH_PROFILE_FOLLOW,
+  FETCH_PROFILE_UNFOLLOW
+} from "@/store/actions.type";
+
+export default {
+  name: "RwvArticleActions",
+  props: {
+    article: { type: Object, required: true },
+    canModify: { type: Boolean, required: true }
+  },
+  computed: {
+    ...mapGetters(["profile", "isAuthenticated"])
+  },
+  methods: {
+    toggleFavorite() {
+      if (!this.isAuthenticated) {
+        this.$router.push({ name: "login" });
+        return;
+      }
+      const action = this.article.favorited ? FAVORITE_REMOVE : FAVORITE_ADD;
+      this.$store.dispatch(action, this.article.slug);
+    },
+    toggleFollow() {
+      if (!this.isAuthenticated) {
+        this.$router.push({ name: "login" });
+        return;
+      }
+      const action = this.article.following
+        ? FETCH_PROFILE_UNFOLLOW
+        : FETCH_PROFILE_FOLLOW;
+      this.$store.dispatch(action, {
+        username: this.profile.username
+      });
+    },
+    async deleteArticle() {
+      try {
+        await this.$store.dispatch(ARTICLE_DELETE, this.article.slug);
+        this.$router.push("/");
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+};
+</script>
